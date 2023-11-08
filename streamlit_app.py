78b251866a13f6e2f0cf8095c792559e46dec243
recnepsc <cspencer@providox.com>
2023-11-07 19:58:08 -0500

    Update streamlit_app.py
    added fruit table insertion

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,5 @@ streamlit.dataframe(my_data_rows)
 
 second_fruit_choice = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
 streamlit.write('Thanks for adding ', second_fruit_choice)
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
